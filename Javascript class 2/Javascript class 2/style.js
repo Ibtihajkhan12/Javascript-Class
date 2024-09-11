a6b7c3641b67d6f9cdec06c1c3f81fc21d6d8beb
Ibtihaj Khan <Ibtihaajkhaan@gmail.com>
2024-09-11 21:12:12 +0500

    Add files via upload

---
+++ Javascript class 2/Javascript class 2/style.js
@@ -0,0 +1,31 @@
+
+// let English = (prompt ("Please Enter Your English Number")) 
+// let Math= (prompt ("Please Enter Your Math Number"))
+// let Urdu = (prompt ("Please Enter Your Urdu Number"))
+// let Islamiat = (prompt ("Please Enter Your Islamiat Number"))
+// let Pst = (prompt ("Please Enter Your Pst Number"))
+
+// let totalMarks = 500; obtainedMarks = English + Urdu + Math + Islamiat + Pst;
+
+// let Percentage = ((obtainedMarks/totalMarks)*100); grade;
+
+// if(Percentage >=80){
+//     grade ="A+"
+// }else if(Percentage >=70){
+//     grade ="A"
+// }
+// else if(Percentage >=60){
+//     grade ="B"
+// }
+// else if(Percentage >=50){
+//     grade ="C"
+// }else{
+//     grade ="F"
+// }
+
+// alert ("English + Urdu + Math + Islamiat +Pst");
+
+var firstNumber = 5
+var secondNumber = 3
+
+alert ("firstNumber + SecondNumber")
